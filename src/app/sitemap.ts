c5aa0f1d957a47a4a4ae9e05c6f7c37611764703
Aryan <aryandps06@gmail.com>
2026-06-26 21:39:33 +0530

    Update sitemap URL

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://project-aether-16l8.vercel.app",
+      url: "https://project-aether-16l8.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
